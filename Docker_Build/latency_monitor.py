a17b5e9262259775f31fae4103a26304441fc928
Sven Holz <sholz@pixelsau.athome.isven.de>
2022-06-13 23:35:46 +0200

    inital upload

---
+++ Docker_Build/latency_monitor.py
@@ -0,0 +1,171 @@
+import sys
+import os
+import datetime
+import configparser
+from threading import *
+from time import *
+from pythonping import ping
+# pip3 install influxdb-client
+from influxdb_client import InfluxDBClient, Point, WriteOptions, WritePrecision
+
+class MyInfluxDB():
+    """
+    Class for InfluxDB connections.
+    
+        Reads config from *config.ini*:
+            [influx]
+            infx_url = Influx2 URL like: *http://10.0.0.1:8086* (must)
+            infx_token = Influx2 Token for corresponding bucket (must)
+            infx_bucket = Influx2 Bucket (must)
+            infx_org = Influx2 Organization (must)
+
+    Attributes
+    ----------
+        n.a.
+
+    Methods
+    -------
+        write(host(string), host_location(string), ping_response(string))
+            writes received data to Influxdb:
+                host: string which includes IP or FQDN
+                host_location: string which includes location of the host
+                ping_response: string which includes the ping reply in ms
+    """
+
+    def __init__(self):
+
+        ## IF ENVIRONMENT VARIABLES ARE PASSED IGNORE CONFIG FILE
+        if 'INFLUX_URL' in os.environ:
+
+            INFX_URL = os.environ['INFLUX_URL']
+            INFX_TOKEN = os.environ['INFLUX_TOKEN']
+            INFX_BUCKET = os.environ['INFLUX_BUCKET']
+            INFX_ORG = os.environ['INFLUX_ORG']
+            INFX_BATCH = os.getenv('INFLUX_BATCH', 60)   # if not set use default
+            INFX_FINT = os.getenv('INFLUX_FINT', 30_000) # if not set use default
+            INFX_JINT = os.getenv('INFLUX_JINT', 5_000)  # if not set use default
+            INFX_RINT = os.getenv('INFLUX_RINT', 5_000)  # if not set use default
+            
+        else:
+
+            config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+            config = configparser.ConfigParser()
+            config.read(config_file)
+
+            INFX_URL = config['influx']['infx_url']
+            INFX_TOKEN = config['influx']['infx_token']
+            INFX_BUCKET = config['influx']['infx_bucket']
+            INFX_ORG = config['influx']['infx_org']
+            INFX_BATCH = config.get('influx', 'infx_batch_size', fallback=60)        # if not set use default
+            INFX_FINT = config.get('influx', 'infx_flush_interval', fallback=30_000) # if not set use default 
+            INFX_JINT = config.get('influx', 'infx_jitter_interval', fallback=5_000) # if not set use default
+            INFX_RINT = config.get('influx', 'infx_retry_interval', fallback=5_000)  # if not set use default
+
+        # This one is needed in our methods    
+        self.INFX_BUCKET = INFX_BUCKET
+
+        # create influxdb client
+        self.client = InfluxDBClient(url=INFX_URL,
+                                     token=INFX_TOKEN,
+                                     org=INFX_ORG)
+
+        # create influxdb write api
+        self.write_api = self.client.write_api(write_options=WriteOptions(batch_size=INFX_BATCH,
+                                                                          flush_interval=INFX_FINT,
+                                                                          jitter_interval=INFX_JINT,
+                                                                          retry_interval=INFX_RINT))
+
+    def __del__(self):
+        self.client.close()
+
+    def write(self, host, host_location, ping_response):
+        self.host = host
+        self.host_location = host_location
+        self.ping_response = ping_response
+        self.influx_timestamp = int(time_ns())
+        self.data_point = Point("latency_monitor").tag("location", self.host_location).tag("host", self.host).field("latency", self.ping_response).time(self.influx_timestamp)
+        self.write_api.write(bucket=self.INFX_BUCKET,
+                             record=self.data_point, 
+                             write_precision='s')
+
+
+class ThreadPing(Thread):
+    """
+    Class of type thread which *pings* given hosts and passes data to InfluxDB2.
+        - one thread for each ping
+        - passes ping results to given InfluxDB2 
+    
+    Arguments
+    ----------
+        db: InfluxDB Object
+        host: string which includes IP or FQDN
+        host_timer: integer which defines how often pings are send in seconds (min. 1)
+        host_location: string which includes location of the host
+    """
+    def __init__(self, db, host, host_timer, host_location):
+        Thread.__init__(self)
+        self.MyDB = db
+        self.host = host
+        self.host_timer = host_timer
+        self.host_location = host_location
+
+    def run(self):
+        self.starttime = time()
+        while True:
+            self.ping_response_list = ping(self.host, count=1)
+            self.ping_response = self.ping_response_list.rtt_avg_ms
+            self.MyDB.write(self.host, self.host_location, self.ping_response)
+            sleep(self.host_timer - ((time() - self.starttime) % 1))
+    
+
+def main():
+    
+    MyDB = MyInfluxDB()
+    
+    # Place to store running threads...
+    my_threads = []
+
+    ## IF ENVIRONMENT VARIABLES ARE PASSED IGNORE CONFIG FILE
+    if 'TARGET_HOST' in os.environ:
+        host = os.environ['TARGET_HOST']
+        host_timer = int(os.getenv('TARGET_TIMER', 5))
+        host_location = os.getenv('TARGET_LOCATION', 'unknown')
+        
+        # Create Thread
+        print("Creating thread for: %s, with interval: %s and location: %s" %(host, host_timer, host_location))
+        thread = ThreadPing(MyDB, host, host_timer, host_location)
+        my_threads.append(thread)
+        thread.start()
+
+    else:
+
+        ## Read Config file
+        config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        host_items = config.items("hosts")
+
+        # Create thread for each configured host
+        for key, host in host_items:
+
+            # Check if hosts timer is set otherwise use "5" (means 5 seconds)
+            host_timer = int(config.get('hosts_timer', key, fallback=5))
+
+            # Check if hosts location is set otherwise use "unknown"
+            host_location = config.get('hosts_location', key, fallback="unknown")
+
+            # Create Thread
+            print("Creating thread for: %s, with interval: %s and location: %s" %(host, host_timer, host_location))
+            thread = ThreadPing(MyDB, host, host_timer, host_location)
+            my_threads.append(thread)
+            thread.start()
+
+    # Join one child thread otherwise main thread will stop (endless loop is also an option)
+    for thread in my_threads:
+        thread.join()
+
+    
+if __name__ == '__main__':
+    main()
+
+
---
+++ README.md
@@ -0,0 +1,164 @@
+# Docker Based Latency Monitor
+
+Docker container which tracks latency of one or many hosts and reports to InfluxDBv2.
+
+## Description
+
+This Docker Container is able to track the latency of one or many targets and reports all data to a given InfluxDBv2.
+
+It´s based on python3 an makes usage of following python libraries:
+
+- pythonping
+- influxdb_client
+- threading
+- sys
+- os
+- datetime
+- configparser
+- time
+
+Configuration can be passed via ENV **OR** configuration file.
+
+In case of using the ENV option you are just able to monitor **ONE** target for more targets please use the configuration file. 
+
+Also some influx connection options are just configurable via config file but normally they are not needed.
+
+## Getting Started
+
+### Requirements
+
+- Docker
+- Docker-Compose
+- InfluxDB Version >= 2
+- pythonping needs root privileges so same for the container
+
+### ENV Variables
+
+Name | Example | Usage | Option/Must
+:------: | :-----: | :-----: | :-----:
+INFLUX_URL | http://10.0.0.1:8086 | InfluxDB Host | must
+INFLUX_TOKEN | eWOcp-MCv2Y3IJPlER7wcCiuYkV8HdC8d7...ICKirhw0lwEczRNnrIoTqZAg== | InfluxDB API Token | must
+INFLUX_BUCKET | latency | InfluxDB Bucket | must
+INFLUX_ORG | MyOrg | InfluxDB Organization | must
+TARGET_HOST | 8.8.8.8 | Monitored Host (IP/FQDN) | must
+
+### Config File
+
+**Instead** of using the ENV variables you can use a config file.
+
+**Keep in mind it´s a OR decision not a AND**  
+
+See [template_config.ini](./Docker_Build/template_config.ini)
+
+Rename the file to *config.ini* make your changes and add it as a volume mount to the container:
+
+#### Docker-Compose Style
+
+```
+        volumes:
+            - /YOUR_PATH/config.ini:/app/config.ini
+```
+
+#### Docker-CLI Style
+
+```
+            docker latency-monitor -v /YOUR_PATH/config.ini:/app/config.ini
+```
+
+### Compose Files
+
+#### FULL-STACK
+
+1st thing to do is creating the *docker-compose.yml:
+
+```
+cp docker-compose-full_stack.yml docker-compose.yml
+```
+
+##### Certificate
+
+*Traefik* will act as a proxy and ensures the usage of TLS so it needs your certificate and key file.
+
+within the *docker-compose.yml* you will find:
+
+```
+      - ./traefik/mycert.crt:/certs/cert.crt:ro
+      - ./traefik/mycert.key:/certs/privkey.key:ro
+```
+
+so please place your certificate file as *./traefik/mycert.crt* and the key file as *./traefik/mycert.key*.
+
+Thats it
+
+##### Variables
+You need to configure Variables in following files to make the compose work:
+
+- **file**
+  - VARIABLE1
+  - VARIABLE2
+  - VARIABLE3
+
+-----
+
+- **docker-compose.yml** *(was docker-compose-full_stack.yml before*
+    - PLACE_YOUR_FQDN_HERE (3 times)
+
+-----
+
+- **.env** *(env needs to be renamed to .env)*
+    - YOUR_PATH_TO_CONTAINER_STATIC_DATA
+    - YOUR_ADMIN_USER
+    - YOUR_ADMIN_PASSWORD
+    - YOUR_ORGANIZATION
+    - YOUR_BUCKET_NAME
+    - YOUR_ADMIN_TOKEN
+
+-----
+
+- **grafana/provisioning/datasources/grafana-datasource.yml**
+    - YOUR_ADMIN_TOKEN
+    - YOUR_ORGANIZATION
+    - YOUR_BUCKET_NAME
+
+
+##### File Permissions
+Because we are configuring *grafana* for permanent data storing and *grafana* actually runs with *UID* + *GID*: *472:472* it´s necessary to change permisson of die permanent storage directory we have configured.
+
+The directory build from the following config part of grafana within the docker-compose.yml:
+
+```
+${MyPath}/grafana/var_lib
+```
+
+*MyPath* was configured earlier in the *.env* file.
+
+so let´s assume the following:
+
+MyPath = /opt/docker/containers/
+
+then you have to do the following
+
+```
+chown -R 472:472 /opt/docker/containers/grafana/var_lib
+```
+
+
+
+
+
+
+## Authors
+
+Contributors names and contact info
+
+* [Sven Holz](mailto:code+latency-monitor@planet-espresso.com)
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## License
+
+free to use
+
